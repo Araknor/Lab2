ec23a61342069ad311ac1bb29960613704445ef8
Gabriel Bisson <gabbis153@gmail.com>
2015-01-30 11:51:32 -0500

    2 tests fonction

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -1,1 +1,16 @@
 #include "dynamicArray.h"
+
+DynamicArray::DynamicArray(unsigned index)
+{
+	capacite = index;
+}
+
+int DynamicArray::getCapacite() const
+{
+	return capacite;
+}
+
+int DynamicArray::getElement(int i) const
+{
+	return 0;
+}
--- DynamicArray/dynamicArray.h
+++ DynamicArray/dynamicArray.h
@@ -6,12 +6,22 @@ class DynamicArray
 {
 
 public:
-
-
-
+	DynamicArray() :
+		capacite(100)
+	{
+		tabElements = new int[capacite];
+		for (int i = 0; i < capacite; i++)
+		{
+			tabElements[i] = 0;
+		}
+	}
+	DynamicArray(unsigned int index);
+	int getCapacite() const;
+	int getElement(int i) const;
+	//void setElement(const int index_element, const int nouvelle_valeur_element);
 private:
-
-
+	int *tabElements;
+	unsigned int capacite;
 };
 
 
